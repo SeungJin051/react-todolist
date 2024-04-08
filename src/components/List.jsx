59c8c382b7e9764bc1ce59cf85ea8369179d47b1
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-04-08 23:19:16 +0900

    Read - 투두리스트 렌더링하기
    filter와 includes로 검색 기능 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-export default function List() {
+export default function List({ todos }) {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    // filter 메소드 내부에서 todo.content.includes(search)의 결과를 반환합니다.
+    return todos.filter((todo) => {
+      return todo.content.includes(search); // 명시적으로 true 또는 false를 반환합니다.
+    });
+  };
+
+  const fillteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🔥</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        placeholder="검색어를 입력하세요"
+        value={search}
+        onChange={onChangeSearch}
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {fillteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
